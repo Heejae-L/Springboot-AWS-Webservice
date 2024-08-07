a5192bebeb505ca14c945ef071afbd3fa9e1695b
Heejae-L <leeheejae@gachon.ac.kr>
2024-08-07 10:58:03 +0900

    first commit

---
+++ src/main/java/com/spring/aws/springbootwebservice/SpringbootAwsWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aws.springbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/aws/springbootwebservice/SpringbootAwsWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.aws.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
